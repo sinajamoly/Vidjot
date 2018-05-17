b3ba22638321d20f9995b34dc1ef09e00fefb777
sinajamoly <sinajamoly@gmail.com>
2018-05-17 16:18:43 -0700

    view has been added

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 
 const app =express();
+//Map global promise
+mongoose.Promise =  global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    // useMongoClient: true
+})
+    .then(()=>console.log('mongoDB Connected'))
+    .catch(err => console.log(err));
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//middleware
+app.use(function(req, res, next){
+    req.name = 'brad Tra'
+    next();
+});
 
 //index Route
 app.get('/',(req, res)=>{
-    res.send('Index');
+    const title = 'Welcome';
+    res.render('index', {
+        title
+    });
 });
 
 app.get('/about', (req,res)=>{
-    res.send('About')
+    res.render('About')
 });
 
 app.get('/home', (req,res)=>{
